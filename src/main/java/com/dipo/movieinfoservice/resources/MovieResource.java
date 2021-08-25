c0ce10caa7501358091dd71759fcd582a2b85990
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-25 10:07:15 +0100

    First model and resourse

---
+++ src/main/java/com/dipo/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,14 @@
+package com.dipo.movieinfoservice.resources;
+
+import com.dipo.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/move")
+public class MovieResource {
+
+    public Movie getMovieInfo(){
+
+    }
+}
