9e03f082a8faa0bc2244aab744a066d10291f93e
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-28 16:21:48 +0100

    MovieResource update.

--- src/main/java/com/dipo/movieinfoservice/resources/MovieResource.java
+++ src/main/java/com/dipo/movieinfoservice/resources/MovieResource.java
@@ -1,14 +1,28 @@
 package com.dipo.movieinfoservice.resources;
 
 import com.dipo.movieinfoservice.models.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/move")
+@RequestMapping("/movies")
 public class MovieResource {
 
-    public Movie getMovieInfo(){
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
     }
+
 }
